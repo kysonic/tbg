392e984935a6197a24a7a319a0ab151f1bce9db9
miroshnichnenko.ai <miroshnichenko.ai@borauto.ru>
2025-03-28 21:08:50 +0300

    feat: basic scene

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-    server: { https: true, host: true, port: 3003 },
+    server: { host: true, port: 3003 },
     plugins: [
         tsconfigPaths(),
     ],
